569ff5480796f34de4300e99ebf874cd940c89c3
Edson Santos <edson.santos@gmail.com>
2023-08-14 23:34:15 -0300

    Ajutando a porta

--- api/main.go
+++ api/main.go
@@ -24,31 +24,31 @@ type currency struct {
 }
 
 type initial_currency struct {
-	amount           float32
-	conversion_rate  float32
-	from_currency    string
-	to_currency      string
+	amount          float32
+	conversion_rate float32
+	from_currency   string
+	to_currency     string
 }
 
-type data_exit struct{
+type data_exit struct {
 	Valor_convertido float32 `json:"valorConvertido"`
 	Simbolo_moeda    string  `json: "simboloMoeda"`
 }
 
 func dbConn() (db *sql.DB) {
-    dbDriver := "mysql"
-    dbUser := "apirestgo"
-    dbPass := "t3MzFHDD0Fvd"
-    dbName := "apirestgo"
+	dbDriver := "mysql"
+	dbUser := "apirestgo"
+	dbPass := "t3MzFHDD0Fvd"
+	dbName := "apirestgo"
 	dbHost := "databak"
-    db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+")/"+dbName)
-    if err != nil {
-        panic(err.Error())
-    }
-    return db
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+")/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
 
-func insertDB(currency currency) bool{
+func insertDB(currency currency) bool {
 
 	db := dbConn()
 
@@ -56,7 +56,7 @@ func insertDB(currency currency) bool{
 	if err != nil {
 		panic(err.Error())
 	}
-	
+
 	insForm.Exec(
 		currency.UUID,
 		currency.amount,
@@ -71,15 +71,14 @@ func insertDB(currency currency) bool{
 	return true
 }
 
-
-func convertCurrency(currency initial_currency) (float32){
+func convertCurrency(currency initial_currency) float32 {
 
 	converted_amount := currency.amount * currency.conversion_rate
 
 	return converted_amount
 }
 
-func getRoot(w http.ResponseWriter, r *http.Request){
+func getRoot(w http.ResponseWriter, r *http.Request) {
 
 	var currency_symbol = ""
 
@@ -101,16 +100,16 @@ func getRoot(w http.ResponseWriter, r *http.Request){
 	}
 
 	switch currencyToConvert.to_currency {
-    case "USD":
-        currency_symbol = "$"
-    case "BRL":
-        currency_symbol = "R$"
-    case "EUR":
-        currency_symbol = "\u20ac"
+	case "USD":
+		currency_symbol = "$"
+	case "BRL":
+		currency_symbol = "R$"
+	case "EUR":
+		currency_symbol = "\u20ac"
 	case "BTC":
 		currency_symbol = "\u20bf"
-    }
-	
+	}
+
 	de := data_exit{
 		converted_amount,
 		currency_symbol,
@@ -129,11 +128,11 @@ func getRoot(w http.ResponseWriter, r *http.Request){
 	w.Write(jsonResp)
 }
 
-func getCurrency(e string) (initial_currency){
+func getCurrency(e string) initial_currency {
 
 	s := strings.Split(e, "/")
 
-	if(s[1] != "exchange"){
+	if s[1] != "exchange" {
 		panic("Wrong URL")
 	}
 
@@ -159,17 +158,16 @@ func getCurrency(e string) (initial_currency){
 	return c
 }
 
-
-func main(){
+func main() {
 
 	fmt.Println("Currency conversion started")
 
 	http.HandleFunc("/", getRoot)
 
-	err := http.ListenAndServe(":7000", nil)
+	err := http.ListenAndServe(":8000", nil)
 
 	if err != nil {
 		panic(err.Error())
 	}
-	
+
 }
